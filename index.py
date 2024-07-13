48b1b02c43b62bf0415404a764853c8f50cf8d34
MChandraR <2201020103@student.umrah.ac.id>
2024-07-13 15:14:20 +0700

    Update index.py

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ if uploaded_file is not None:
     img = np.array(image)
     img = img[:, :, ::-1].copy()
     image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     faces = face_cascade.detectMultiScale(image, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
